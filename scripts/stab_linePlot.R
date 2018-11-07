40e4ccd75ea71e53b022bb04405e39c45e9f1f04
zhxiaokang <zhxiaokang@gmail.com>
2018-11-07 15:47:01 +0100

    change font size

--- scripts/stab_linePlot.R
+++ scripts/stab_linePlot.R
@@ -32,13 +32,13 @@ num.fea <- ncol(data.raw) - 1  # number of features, but notice that the last co
 nums.sel.fea <- ceiling(num.fea * percent.sel.fea)  # the numbers of selected features
 
 df.merge <- data.frame()  # to record the final merged dataframe
-names.methods <- c('SAM', 'GeoDE', 'ReliefF', 'Information Gain', 'Function Perturbation', 'EFSIS')
+names.methods <- c('SAM', 'GeoDE', 'ReliefF', 'Information Gain', 'Function Perturbation','EFSIS')
 stabs <- matrix(nrow = length(nums.sel.fea), ncol = length(names.methods))
 j <- 0
 for (i in nums.sel.fea) {
   j = j + 1
   file.name <- paste(path.data, 'num', i, '-stab-', script.version, '.txt', sep = '')
-  stab <- read.table(file.name)[-7,]  # read the file
+  stab <- read.table(file.name)[c(1:5, 7), ]  # read the file, skip the line for 'EFSOS'
   stabs[j, ] <- unlist(stab)
 }
 
@@ -46,8 +46,12 @@ df.stabs <- as.data.frame.matrix(stabs)
 colnames(df.stabs) <- names.methods
 df.stabs$num.sel.fea <- nums.sel.fea
 stabs.long <- melt(df.stabs, id = 'num.sel.fea')
-pic <- ggplot(data = stabs.long, aes(x = num.sel.fea, y = value, linetype = variable, colour = variable)) + geom_line() + labs(x = 'Number of selected features', y = 'Stability', title = data.set)
+pic <- ggplot(data = stabs.long, aes(x = num.sel.fea, y = value, linetype = variable, colour = variable, shape = variable)) + 
+  geom_line(size = 1) + geom_point(size = 2.5) + labs(x = 'Number of selected features', y = 'Stability', title = data.set) + 
+  theme_bw() + theme(plot.title = element_text(hjust = 0.5, size = 15, face = "bold"), 
+                     axis.text=element_text(size=15, face = 'bold'), axis.title=element_text(size=15, face = 'bold'), 
+                     legend.text=element_text(size=12), legend.title = element_blank())
 
-pdf(file = paste('../fig/stab-', data.set, '-', script.version, '.pdf', sep = ''))
+pdf(file = paste('../fig/stab-', data.set, '-', script.version, '.pdf', sep = ''), width = 10, height = 7)
 pic
 dev.off()
