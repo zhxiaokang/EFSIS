a259e51cb4ff492ea0affd2ab307e297de02fb16
zhxiaokang <zhxiaokang@gmail.com>
2019-10-08 20:07:14 -0700

    func to add column to empty df and matrix

---
+++ scripts/cbind.all.R
@@ -0,0 +1,8 @@
+# define the function to add column to an empty dataframe / matrix
+cbind.all <- function(...){
+  nm <- list(...) 
+  nm <- lapply(nm, as.matrix)
+  n <- max(sapply(nm, nrow)) 
+  do.call(cbind, lapply(nm, function (x) 
+    rbind(x, matrix(, n-nrow(x), ncol(x))))) 
+}
